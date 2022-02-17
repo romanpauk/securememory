af09a68166ba0f9e67f0c3153e32a3090ea7a237
Roman Paukner <romanpauk@gmail.com>
2022-02-17 12:54:39 +0100

    finer-grained locking to ram

--- include/securememory/win32/heap.h
+++ include/securememory/win32/heap.h
@@ -1,5 +1,6 @@
 #include <cassert>
 #include <stdexcept>
+#include <mutex>
 
 namespace securememory::win32
 {
@@ -35,29 +36,26 @@ namespace securememory::win32
         };
        
     public:
-        heap(size_t size)
-            : heap_(HeapCreate(0, size, size), heap_deleter())
+        heap(std::size_t reserve)
+            : heap_(HeapCreate(0, get_page_size(), reserve), heap_deleter())
             , base_address_()
-            , size_(size)
+            , size_(reserve)
+            , heap_page_refs_(std::max(std::size_t(1), reserve / get_page_size()))
         {
+            // TODO: get heap size properly by walking heap
             base_address_ = init_base_address();
                     
-            // Resize the working set according to size requested.
+            // TODO:
+            // Resize the working set according to heap size.
             SIZE_T dwMin, dwMax;
             if (!GetProcessWorkingSetSize(GetCurrentProcess(), &dwMin, &dwMax))
             {
                 throw exception("GetProcessWorkingSetSize", GetLastError());
             }
 
-            if (!SetProcessWorkingSetSize(GetCurrentProcess(), dwMin + size_ + 0, dwMax + size_ + 0))
+            if (!SetProcessWorkingSetSize(GetCurrentProcess(), dwMin + reserve + 0, dwMax + reserve + 0))
             {
                 throw exception("SetProcessWorkingSetSize", GetLastError());
-            }
-
-            // Lock the heap pages
-            if (VirtualLock(base_address_, size_) == FALSE)
-            {                
-                throw exception("VirtualLock", GetLastError());
             }
         }
 
@@ -72,18 +70,79 @@ namespace securememory::win32
         void* allocate(std::size_t size)
         {
             assert(heap_ != NULL);
-            return HeapAlloc(heap_.get(), 0, size);
+
+            std::lock_guard< std::mutex > lock(mutex_);
+
+            void* ptr = HeapAlloc(heap_.get(), 0, size);
+            if (ptr)
+            {
+                uintptr_t pagemask = ~(get_page_size() - 1);
+                uintptr_t address = reinterpret_cast<uintptr_t>(ptr) & pagemask;
+                uintptr_t base = base_address() & pagemask;
+
+                std::size_t pages = std::max(std::size_t(1), size / get_page_size());
+                for (size_t i = 0; i < pages; ++i, address += get_page_size())
+                {
+                    uintptr_t index = (address - base) / get_page_size();
+                    if (heap_page_refs_[index] == 0)
+                    {                        
+                        if (VirtualLock(reinterpret_cast<LPVOID>(address), get_page_size()))
+                        {
+                            ++heap_page_refs_[index];
+                        }
+                        else
+                        {
+                            deallocate(ptr, size);
+
+                            // TODO: not sure about the exception
+                            throw exception("VirtualLock", GetLastError());
+                        }                       
+                    }
+                }
+            }
+
+            return ptr;
         }
 
         void deallocate(void* ptr, std::size_t size)
         {
             assert(heap_ != NULL);
 
-            SecureZeroMemory(ptr, size);
-            
-            if (HeapFree(heap_.get(), 0, ptr) == FALSE)
+            if (ptr)
             {
-                throw exception("HeapFree", GetLastError());
+                SecureZeroMemory(ptr, size);
+
+                uintptr_t pagemask = ~(get_page_size() - 1);
+                uintptr_t address = reinterpret_cast<uintptr_t>(ptr) & pagemask;
+                uintptr_t base = base_address() & pagemask;
+
+                std::lock_guard< std::mutex > lock(mutex_);
+
+                std::size_t pages = std::max(std::size_t(1), size / get_page_size());
+                for (size_t i = 0; i < pages; ++i, address += get_page_size())
+                {
+                    uintptr_t index = (address - base) / get_page_size();
+                    if (heap_page_refs_[index] == 1)
+                    {
+                        if (VirtualUnlock(reinterpret_cast<LPVOID>(address), get_page_size()))
+                        {
+                            --heap_page_refs_[index];
+                        }
+                        else
+                        {
+                            // TODO: not sure about the exception. 
+                            // But the fact that we failed to unlock the page may be serious.
+                            // This code has issues with multiple exceptions.
+                            throw exception("VirtualUnlock", GetLastError());
+                        }                       
+                    }
+                }
+
+                // TODO: on scope exit, free
+                if (HeapFree(heap_.get(), 0, ptr) == FALSE)
+                {
+                    throw exception("HeapFree", GetLastError());
+                }
             }
         }
 
@@ -91,11 +150,18 @@ namespace securememory::win32
         {
             return reinterpret_cast< uintptr_t >(base_address_);
         }
-
-        // Used in allocator::max_size(), yet this is slightly bigger than max size due to heap overhead.
-        std::size_t size() const
+                
+        std::size_t locked_size() const
         {
-            return size_;
+            std::lock_guard< std::mutex > lock(mutex_);
+
+            std::size_t count = 0;
+            for (auto& ref : heap_page_refs_)
+            {
+                count += ref > 0;
+            }
+
+            return count * get_page_size();
         }
 
     private:
@@ -109,9 +175,8 @@ namespace securememory::win32
             while (HeapWalk(heap_.get(), &entry) != FALSE)
             {
                 if ((entry.wFlags & PROCESS_HEAP_REGION) != 0)
-                {
-                    assert(entry.Region.dwUnCommittedSize == 0);
-                    assert(entry.Region.dwCommittedSize >= size_);
+                {                    
+                    assert(entry.Region.dwUnCommittedSize + entry.Region.dwCommittedSize >= size_);
                     assert(base_address_ == 0);
                     
                     base = entry.lpData;
@@ -132,7 +197,17 @@ namespace securememory::win32
             return base;
         }
 
-        std::unique_ptr< HANDLE, heap_deleter > heap_;
+        static uintptr_t get_page_size()
+        {
+            static uintptr_t page_size = []() { SYSTEM_INFO si; GetSystemInfo(&si); return si.dwPageSize; }();
+            return page_size;
+        }
+        
+        // TODO: this mutex is on very defensive side. It should be possible to use atomic refcounts at least.
+        mutable std::mutex mutex_;
+        std::unique_ptr< HANDLE, heap_deleter > heap_;        
+        std::vector< uint32_t > heap_page_refs_;
+
         LPVOID base_address_;
         std::size_t size_;
     };
--- test/securememory.cpp
+++ test/securememory.cpp
@@ -9,11 +9,30 @@ BOOST_AUTO_TEST_CASE(heap_test)
 
 BOOST_AUTO_TEST_CASE(allocator_test)
 {
-	securememory::win32::heap heap(1 << 10);
+	securememory::win32::heap heap(1 << 20);
 	securememory::allocator< char > allocator(&heap);
 	
-	char *p = allocator.allocate(10);
-	allocator.deallocate(p, 10);
+	BOOST_CHECK(heap.locked_size() == 0);
+
+	const size_t count = 64;
+	const size_t size = 1000;
+	std::vector< char* > ptrs;
+	for (size_t i = 0; i < count; ++i)
+	{
+		char* p = allocator.allocate(size);
+		std::cerr << "allocate: " << i << ": locked " << heap.locked_size() << std::endl;
+		ptrs.push_back(p);
+	}
+
+	BOOST_CHECK(heap.locked_size() > 0);
+
+	for (size_t i = 0; i < count; ++i)
+	{
+		allocator.deallocate(ptrs[i], size);
+		std::cerr << "deallocate: " << i << ": locked " << heap.locked_size() << std::endl;
+	}
+
+	BOOST_CHECK(heap.locked_size() == 0);
 }
 
 BOOST_AUTO_TEST_CASE(vector_test)
@@ -42,4 +61,6 @@ BOOST_AUTO_TEST_CASE(map_test_global)
 	// This just tests that the code compiles with the global_heap() function.
 	std::map< int, std::string, std::less< int >, securememory::allocator< std::pair< const int, std::string > > > map;
 	map.emplace(1, "aaa");
+
+	std::vector< std::string, securememory::allocator < std::string > > vec;
 }
